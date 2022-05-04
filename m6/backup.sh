6d9de3482a4e0bc86519e8f3ff8e6a10704b41e7
Yevhen Lytviak <ylytviak@gmail.com>
2022-05-05 00:56:46 +0300

    partially part C without cron

--- m6/backup.sh
+++ m6/backup.sh
@@ -7,28 +7,32 @@
 
 #In case of adding new or deleting old files, the script must add a corresponding entry to the log file indicating the time, type of operation and file name. [The command to run the script must be added to crontab with a run frequency of one minute] 
 
-if [ -z $* ]
-then
-echo "No options found!"
-exit 1
-fi
-
 checkargs () {
-if [[ $OPTARG =~ ^-[a/b]$ ]]
+if [[ $OPTARG =~ ^-[i/o]$ ]]
 then
 echo "Unknow argument $OPTARG for option $opt!"
 exit 1
 fi
 }
 
-
-while getopts "a:b:" opt
+while getopts "i:o:" opt
 do
 case $opt in
-a) checkargs; 
-	echo "$OPTARG";;
-b) checkargs;
-	echo "$OPTARG";;
+i) checkargs; 
+	from="$OPTARG";;
+o) checkargs;
+	to="$OPTARG";;
 *) echo "No reasonable options found!";;
 esac
 done
+echo "$from"
+echo "$to"
+# 
+rsync -acr --log-file=diff.log "$from" "$to"
+#diff "$from" "$to" | awk -F: '{ print $2 }' | awk '{ print '$from'$1 }' > list   
+#diff "$from" "$to" | awk -F: '{ print $2 }' > list
+#tst=/sdf/sdf/; cat test | awk -b '{ "%s, %s" { printf '$tst'$1 } }' > for
+#cat list
+#cat list | xargs cp -t "$to"
+#cat list
+#awk -F: '{ print $2 }' source | awk '{ print $1 }'
--- m6/readme.md
+++ m6/readme.md
@@ -94,3 +94,7 @@ Create a data backup script that takes the following data as parameters:
 
 In case of adding new or deleting old files, the script must add a corresponding entry to the log file indicating the time, type of operation and file name. [The command to run the script must be added to crontab with a run frequency of one minute]
 
+Simpliest way is to use rsync:
+``` rsync -acr --log-file=diff.log "$from" "$to" ```
+send to cron by
+
