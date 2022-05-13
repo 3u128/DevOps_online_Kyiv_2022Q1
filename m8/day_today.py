cc23ea0eb6a6c18f593a5e3b506cce49aac1e6df
Yevhen Lytviak <ylytviak@gmail.com>
2022-05-13 20:36:02 +0300

    add comments

--- m8/day_today.py
+++ m8/day_today.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+# https://docs.python.org/3/library/datetime.html
+# 1. Write easy program, which will display current date and time.
+
 from datetime import date
 
 today = date.today()
